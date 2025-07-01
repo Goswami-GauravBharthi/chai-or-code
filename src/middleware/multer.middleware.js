b5393426c7a2db548d7ba5df6f147ddf9be3054d
goswami-gauravbharthi <goswamigaurav210@gmail.com>
2025-07-01 08:22:30 +0530

    comment add for multer

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -2,14 +2,16 @@ import multer, { diskStorage } from "multer";
 
 const storage = diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/tmp");
+    cb(null, "./public/tmp"); 
   },
   filename: function (req, file, cb) {
-    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname);
+    const fileName = `${Date.now()}-${file.originalname}`;
+
+    cb(null, fileName);
   },
 });
 
 export const upload = multer({ storage });
 
 // storage = diskStorage({ destination , filename })
+//destination ==> where you want to upload image
